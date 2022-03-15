821381a1fdfc5464b0c284867208e57ebd032505
alfercar <alfonsofdez97@gmail.com>
2022-03-15 17:23:06 +0100

    traductor

---
+++ Traductor/nls/es/strings.js
@@ -0,0 +1,6 @@
+define({
+    "title": "Primer título",
+    "label1": "Esta etiqueta",
+    "label2": "Me cago en la leche Merche",
+
+})
---
+++ Traductor/nls/strings.js
@@ -0,0 +1,11 @@
+﻿define({
+    root:({
+        //aqui se dicen las propiedades y el valor en ingles
+        title: "First title",
+        label1: "This label",
+        label2: "I shit on the milk Merche"
+        
+    }) ,
+    "es": 1
+    //Hay que poner un :1 por cada carpeta que es como un true/false siendo el 1 como true
+});
